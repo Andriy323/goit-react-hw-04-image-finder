04812c117ae61cb03f8eed8b0934b819238b8ffd
Andriy323 <108237550+Andriy323@users.noreply.github.com>
2023-02-05 21:53:03 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,34 +1,32 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 
 import css from 'components/Modal/modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
-
-class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.modalClose);
-  }
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.modal);
-  }
-
-  modalClose = ({ target, currentTarget, code }) => {
+const Modal = ({ close, children }) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const modalClose = ({ target, currentTarget, code }) => {
     if (target === currentTarget || code === 'Escape') {
-      this.props.close();
+      close();
     }
   };
-  render() {
-    const { children } = this.props;
-    return createPortal(
-      <div className={css.overlay} onClick={this.modalClose}>
-        <div className={css.modal}>{children}</div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  useEffect(() => {
+    document.addEventListener('keydown', modalClose);
+
+    return () => {
+      document.removeEventListener('keydown', modalClose);
+    };
+  }, [modalClose]);
+
+  return createPortal(
+    <div className={css.overlay} onClick={modalClose}>
+      <div className={css.modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
+};
 
 export default Modal;
 Modal.propTypes = {
